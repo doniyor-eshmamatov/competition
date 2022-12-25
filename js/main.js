e181dbe7fc9c773b524d46b9d70c0a7f1eeabb98
doniyor-eshmamatov <edoniyorbekdev@gmail.com>
2022-12-25 12:48:30 +0500

    Finished header

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//   DARK mode codes
+
+let elBody = document.querySelector('body');
+let modeBtn = document.querySelector('.linkbox__mode__btn');
+
+modeBtn.addEventListener('click', () => {
+    
+})
