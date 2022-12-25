7965b82720dc2abc56022f1c9558bcc3d2321707
doniyor-eshmamatov <edoniyorbekdev@gmail.com>
2022-12-25 17:10:24 +0500

    Add choose section

---
+++ js/cars.js
@@ -0,0 +1,31 @@
+let compact = [
+    {
+        name: 'Ford Fiesta',
+        type: 'Economy Car',
+        img: '../images/array-images/compact1.png',
+    },
+    {
+        name: 'Nissan Versa',
+        type: 'Compact Car',
+        img: '../images/array-images/compact2.png',
+    },
+    {
+        name: 'Ford Fiesta',
+        type: 'Economy Car',
+        img: '../images/array-images/compact1.png',
+    },{
+        name: 'Ford Fiesta',
+        type: 'Economy Car',
+        img: '../images/array-images/compact1.png',
+    },
+    {
+        name: 'Ford Fiesta',
+        type: 'Economy Car',
+        img: '../images/array-images/compact1.png',
+    },
+    {
+        name: 'Ford Fiesta',
+        type: 'Economy Car',
+        img: '../images/array-images/compact1.png',
+    }
+]
--- js/hero-carousel.js
+++ js/hero-carousel.js
@@ -3,24 +3,31 @@ $('.hero__carousel__list').slick({
     // autoplay: true,
     speed: 3000,
     responsive: [
+        // {
+        //     breakpoint: 1024,
+        //     settings: {
+        //         slidesToShow: 3,
+        //         slidesToScroll: 3,
+        //         infinite: true,
+        //         dots: true
+        //     }
+        // },
+        // {
+        //     breakpoint: 912,
+        //     settings: {
+        //         slidesToShow: 1,
+        //         slidesToScroll: 1
+        //     }
+        // },
         {
-            breakpoint: 1024,
+            breakpoint: 768,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
         },
         {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
+            breakpoint: 576,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
